f98174801c7ac8ff26e2c03da06a5a03fc728b0b
matthewsklar227@gmail.com <matthewsklar227@gmail.com>
2017-08-22 14:52:27 -0400

    Setup action

--- main.py
+++ main.py
@@ -4,11 +4,34 @@ import tensorflow as tf
 
 env = gym.make('CartPole-v0')
 
-
 n_actions = env.action_space.n
 n_states = 4
+
+run_time = 200
+
+epsilon = 0.7  # Probability of doing a random move
+gamma = 0.9  # Discounted future reward
+mb_size = 50  # Minibatch size
 
 if __name__ == '__main__':
     q_matrix = np.zeros([n_states, n_actions])
 
-    print(q_matrix)
+    observation = env.reset()
+    obs = np.expand_dims(observation, axis=0)
+    state = np.stack((obs, obs), axis=1)
+
+    done = False
+
+    for i in range(run_time):
+        env.render()
+
+        if np.random.rand() <= 1:
+            action = np.random.randint(0, env.action_space.n, size=1)[0]
+        else:
+            action = None  # https://github.com/llSourcell/deep_q_learning/blob/master/03_PlayingAgent.ipynb
+
+        observation_new, reward, done, info = env.step(action)
+
+        if done:
+            env.reset()
+    
